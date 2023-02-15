4530d2a940b728088e7557ea81105f90f092089d
John Heher <jheher@gmail.com>
2023-02-15 21:48:19 +0000

    Fixing globe changing in size on scroll

--- app/routes/trip.tsx
+++ app/routes/trip.tsx
@@ -73,7 +73,7 @@ function getGlobeContainerPosition(width, showDetails) {
       return 'bottom-auto top-0';
     }
 
-    return 'bottom-[-20dvh] top-auto';
+    return 'bottom-[-20vh] top-auto';
   }
 
   return 'bottom-0 top-auto';
@@ -102,13 +102,13 @@ function getTopPosition(width, showDetails) {
 function getGlobeHeight(width, routeSelected) {
   if (width < 768) {
     if (routeSelected) {
-      return 'h-[100dvh]';
+      return 'h-[100vh]';
     }
 
-    return 'h-[50dvh]';
+    return 'h-[50vh]';
   }
 
-  return 'h-[100dvh]';
+  return 'h-[100vh]';
 }
 
 export default function Index() {
@@ -170,7 +170,7 @@ export default function Index() {
       <div className="body-container h-[100dvh] max-w-[var(--max-width)] mx-auto">
         {(routeSelected || selectedCity || showDetails) && (
           <>
-            <div className={`globe-background fixed top-0 left-0 z-10 w-full h-[50dvh]`}></div>
+            <div className={`globe-background fixed top-0 left-0 z-10 w-full h-[50vh]`}></div>
             <button
               className={`fixed z-40 top-[10px] left-[10px] w-[40px] h-[40px]`}
               type="button"
