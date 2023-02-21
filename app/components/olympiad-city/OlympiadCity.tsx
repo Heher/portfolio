8cf20f385463cd8e333f6a28e4185d49a3baaa2a
John Heher <jheher@gmail.com>
2023-02-21 22:21:58 +0000

    Fixes up styling

--- app/components/olympiad-city/OlympiadCity.tsx
+++ app/components/olympiad-city/OlympiadCity.tsx
@@ -38,7 +38,7 @@ const SharedOlympiads = ({ olympiads, visit, setSelectedImg }) => {
     <li className="city-olympiad mr-[20px]">
       <div className="title grid grid-cols-[10px_1fr] items-center gap-[7px]">
         <span className={`city-status h-[10px] w-[10px] rounded-full bg-[var(--positive)]`} />
-        <p className="m-0">
+        <p className="m-0 text-[1.2rem]">
           {olympiadYears.join(' and ')}
           {` ${firstOlympiad.olympiadType.charAt(0) + firstOlympiad.olympiadType.slice(1).toLowerCase()} Games`}
         </p>
@@ -79,52 +79,56 @@ export const OlympiadCity = ({
         <motion.div
           ref={cityRef}
           layoutId="expandable-card"
-          className={`olympiad-city selected group fixed top-[25dvh] left-0 z-20 h-[75dvh] w-[100vw] cursor-pointer overflow-scroll border border-solid border-transparent bg-[#e0e0e0] p-[20px]`}
+          className={`olympiad-city selected group fixed top-[25dvh] left-0 z-20 h-[75dvh] w-[100vw] overflow-scroll border border-solid border-transparent bg-[#e0e0e0] p-[20px]`}
         >
-          <motion.div layoutId="expandable-card-header" className="header mt-[20vh]">
-            <div className="flex items-center">
-              <motion.span
-                className={`city-status mr-[10px] h-[15px] w-[15px] rounded-full ${statusColor(
-                  amountCompleted,
-                  totalOlympiads
-                )}`}
-              />
-              <motion.h3 className="text-semibold block text-[2rem] leading-none">{cityInfo.name}</motion.h3>
-            </div>
-            <div className="mt-[8px] flex items-center">
-              <motion.h4 className=" text-bold ml-[25px] text-[1.1rem] uppercase leading-none">
-                {cityInfo.country.name}
-              </motion.h4>
-              <img
-                className="ml-[10px] mt-[2px] h-[15px] w-auto"
-                src={cityInfo.country.flagByTimestamp.png}
-                alt={cityInfo.country.name}
-              />
-            </div>
-          </motion.div>
-          <ul className="mt-[25px] ml-[25px] flex list-none flex-col p-0">
-            {sharedStadiums ? (
-              <SharedOlympiads
-                olympiads={olympiads}
-                visit={visits[olympiads[0].year.toString()]?.[olympiads[0].olympiadType.toLowerCase()]}
-                setSelectedImg={setSelectedImg}
-              />
-            ) : (
-              olympiads.map((olympiad) => {
-                const visit = visits[olympiad.year.toString()]?.[olympiad.olympiadType.toLowerCase()];
+          <motion.div className="mx-auto md:max-w-[800px]">
+            <motion.div layoutId="expandable-card-header" className="header mt-[25vh] md:mt-[15vh]">
+              <div className="flex items-center">
+                <motion.span
+                  className={`city-status mr-[10px] h-[15px] w-[15px] rounded-full ${statusColor(
+                    amountCompleted,
+                    totalOlympiads
+                  )}`}
+                />
+                <motion.h3 className="block text-[2rem] font-semibold uppercase leading-none tracking-wide">
+                  {cityInfo.name}
+                </motion.h3>
+              </div>
+              <div className="mt-[10px] flex items-center">
+                <motion.h4 className="ml-[25px] text-[1.1rem] uppercase leading-none">
+                  {cityInfo.country.name}
+                </motion.h4>
+                <img
+                  className="ml-[10px] mt-[-1px] h-[15px] w-auto"
+                  src={cityInfo.country.flagByTimestamp.png}
+                  alt={cityInfo.country.name}
+                />
+              </div>
+            </motion.div>
+            <motion.ul className="mt-[40px] ml-[25px] flex list-none flex-col p-0">
+              {sharedStadiums ? (
+                <SharedOlympiads
+                  olympiads={olympiads}
+                  visit={visits[olympiads[0].year.toString()]?.[olympiads[0].olympiadType.toLowerCase()]}
+                  setSelectedImg={setSelectedImg}
+                />
+              ) : (
+                olympiads.map((olympiad) => {
+                  const visit = visits[olympiad.year.toString()]?.[olympiad.olympiadType.toLowerCase()];
 
-                return (
-                  <CityOlympiad
-                    key={olympiad.id}
-                    olympiad={olympiad}
-                    visit={visit}
-                    setSelectedImg={setSelectedImg}
-                    selected
-                  />
-                );
-              })
-            )}
-          </ul>
+                  return (
+                    <CityOlympiad
+                      key={olympiad.id}
+                      olympiad={olympiad}
+                      visit={visit}
+                      setSelectedImg={setSelectedImg}
+                      selected
+                    />
+                  );
+                })
+              )}
+            </motion.ul>
+          </motion.div>
         </motion.div>
       ) : (
         <motion.div
@@ -136,10 +140,7 @@ export const OlympiadCity = ({
           } border-l-[15px] border-solid ${statusColor(amountCompleted, totalOlympiads)}`}
           onClick={() => handleCitySelection(cityInfo)}
         >
-          {/* <motion.span
-            className={`city-status mr-[10px] h-full w-[15px] ${statusColor(amountCompleted, totalOlympiads)}`}
-          /> */}
-          <div>
+          <div className="text-[var(--text)]">
             <motion.div layoutId="expandable-card-header" className="header flex items-center">
               <div className="">
                 <motion.h3 className="text-[1.1rem] font-semibold uppercase tracking-wide">{cityInfo.name}</motion.h3>
