a9ff4b1dad012739abca975417f58ad461152bac
John Heher <jheher@gmail.com>
2023-02-22 00:29:02 +0000

    Fixes image modal and some styling

--- app/components/modal/ImageModal.tsx
+++ app/components/modal/ImageModal.tsx
@@ -1,17 +1,26 @@
 import { CloseIcon } from '../icons/CloseIcon';
 
-export const ImageModal = ({ img, closeModal }) => {
+type ImageModalProps = {
+  img: string;
+  closeModal: () => void;
+};
+
+export const ImageModal: React.FC<ImageModalProps> = ({ img, closeModal }) => {
   return (
-    <div className="fixed top-0 left-0 z-50 flex h-[100vh] w-[100vw] items-center justify-center bg-slate-200">
-      <button type="button" onClick={closeModal} className="absolute top-[20px] left-[5vw]">
-        <CloseIcon />
-      </button>
-      <img
-        className="h-auto w-[90vw] border-4 border-solid border-[var(--globe-background)]"
-        src={`https://res.cloudinary.com/globe-draft/image/upload/q_30/olympic-visits/${img}`}
-        alt=""
-        loading="lazy"
-      />
+    <div className="fixed top-0 left-0 z-50 h-[100vh] w-[100vw] bg-slate-200">
+      <div className="relative mx-auto w-full max-w-[var(--max-width)]">
+        <button type="button" onClick={closeModal} className="absolute top-[20px] left-[20px]">
+          <CloseIcon />
+        </button>
+        <div className="flex h-[100vh] items-center justify-center px-[30px]">
+          <img
+            className="h-auto w-full max-w-[600px] border-4 border-solid border-[var(--globe-background)]"
+            src={`https://res.cloudinary.com/globe-draft/image/upload/q_30/olympic-visits/${img}`}
+            alt=""
+            loading="lazy"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- app/routes/trip.tsx
+++ app/routes/trip.tsx
@@ -100,17 +100,17 @@ function GlobeFallback() {
   return <div>Loading...</div>;
 }
 
-function getGlobeContainerPosition(width: number, showDetails: boolean) {
-  if (width < 768) {
-    if (showDetails) {
-      return 'bottom-auto top-0';
-    }
+// function getGlobeContainerPosition(width: number, showDetails: boolean) {
+//   if (width < 768) {
+//     if (showDetails) {
+//       return 'bottom-auto top-0';
+//     }
 
-    return 'bottom-[-20vh] top-auto';
-  }
+//     return 'bottom-[-20vh] top-auto';
+//   }
 
-  return 'top-0 bottom-auto';
-}
+//   return 'top-0 bottom-auto';
+// }
 
 function getBottomPosition(width: number, showDetails: boolean) {
   if (width < 768) {
@@ -274,6 +274,7 @@ export default function TripPage() {
           </>
         )}
         <motion.div
+          // eslint-disable-next-line tailwindcss/classnames-order
           className={`globe-container fixed w-[100vw] ${getGlobeHeight(
             width,
             routeSelected,
@@ -285,7 +286,7 @@ export default function TripPage() {
             moveableGlobe,
             selectedCity
           )} z-30 md:max-h-[800px] lg:max-h-[1000px] ${
-            selectedCity && !moveableGlobe && 'clip-container md:max-h-[500px] md:max-w-[500px] lg:max-h-[500px]'
+            selectedCity && !moveableGlobe && `clip-container md:max-h-[500px] md:max-w-[500px] lg:max-h-[500px]`
           } ${moveableGlobe ? 'md:h-[100vh] md:w-[100vw]' : 'md:h-[90vh] md:w-[90vw]'}`}
           animate={
             width < 768
