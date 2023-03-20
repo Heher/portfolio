fe6acd90c1f98b343aa503dea752697e78bb29a2
John Heher <johnheher@gmail.com>
2023-03-20 00:30:43 -0500

    Updates my location

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 import { GetFlagsDocument } from '~/gql/graphql';
 
 const visitedCountries = [
-  'United States of America',
+  'New Zealand',
   'Great Britain',
   'Bosnia-Herzegovina',
   'Liechtenstein',
@@ -79,7 +79,7 @@ export default function Index() {
           <p>
             Currently in{' '}
             <Link to="/trip" className="text-[var(--cta)] underline" prefetch="intent">
-              Austin, TX
+              Rotorua, NZ
             </Link>
           </p>
         </div>
