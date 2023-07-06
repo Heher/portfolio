1a95b22ac4d140f06694fa9338ffdb12c4ae246a
John Heher <jheher@gmail.com>
2023-07-06 12:49:09 -0500

    First rough draft of full donut page

--- app/components/donut/Donut.tsx
+++ app/components/donut/Donut.tsx
@@ -1,32 +1,39 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { useLoader, useFrame } from '@react-three/fiber';
 import { GLTFLoader } from 'three-stdlib';
 
 import donut from '~/data/donut/donut.gltf';
 
 interface DonutProps {
-  modelPath: string;
   scale?: number;
-  position?: number[];
+  color: number;
 }
 
-export default function Donut({ modelPath, scale = 10, position = [0, 0, 0], color }: DonutProps) {
+export default function Donut({ scale = 10, color }: DonutProps) {
   const ref = useRef();
   const gltf = useLoader(GLTFLoader, donut);
-  console.log(gltf);
+  const [spinDirection, setSpinDirection] = React.useState<'left' | 'right'>('left');
 
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += 0.003));
+  useFrame((state, delta) => {
+    // if (spinDirection === 'left') {
+    //   ref.current.rotation.x -= 0.003;
+    // } else {
+    //   ref.current.rotation.x += 0.003;
+    // }
+
+    // if (ref.current.rotation.x > 0.5) {
+    //   setSpinDirection('right');
+    // } else if (ref.current.rotation.x < -0.5) {
+    //   setSpinDirection('left');
+    // }
+
+    ref.current.rotation.x = Math.sin(state.clock.getElapsedTime() / 2) / 2;
+  });
+
   return (
-    <>
-      <group ref={ref} scale={scale}>
-        <primitive object={gltf.nodes.Donut} position={gltf.nodes.Donut.position} />
-        <primitive
-          object={gltf.nodes.Icing}
-          position={gltf.nodes.Icing.position}
-          material-color={color || gltf.nodes.Icing.material.color}
-        />
-      </group>
-    </>
+    <group ref={ref} scale={scale}>
+      <primitive object={gltf.nodes.Donut} position={gltf.nodes.Donut.position} />
+      <primitive object={gltf.nodes.Icing} position={gltf.nodes.Icing.position} material-color={color} />
+    </group>
   );
 }
--- app/routes/donut/index.tsx
+++ app/routes/donut/index.tsx
@@ -14,17 +14,57 @@ export const meta: MetaFunction = () => ({
   'og:image': '/olympic-cities-og.jpg'
 });
 
+type IcingColor = {
+  baseColor: string;
+  background: string;
+};
+
+type ColorButtonProps = {
+  selectedColor: number;
+  color: IcingColor;
+  handleColorChange: (newValue: string, fromInput?: boolean) => void;
+};
+
 function DonutFallback() {
   return <div>Loading...</div>;
+}
+
+const icingColors = [
+  { baseColor: '#e780ce', background: 'bg-[#e780ce]' },
+  { baseColor: '#9667AB', background: 'bg-[#9667AB]' },
+  { baseColor: '#BA98F0', background: 'bg-[#BA98F0]' },
+  { baseColor: '#DF4335', background: 'bg-[#DF4335]' },
+  { baseColor: '#EBEBE8', background: 'bg-[#EBEBE8]' }
+];
+
+function ColorButton({ selectedColor, color, handleColorChange }: ColorButtonProps) {
+  const selected = `#${selectedColor.toString(16)}` === color.baseColor.toLowerCase();
+
+  return (
+    <button
+      // eslint-disable-next-line tailwindcss/classnames-order
+      className={`h-[28px] w-[28px] rounded-full ${color.background} border-4 ${
+        selected ? 'border-slate-400' : 'border-slate-900'
+      }`}
+      type="button"
+      onClick={() => handleColorChange(color.baseColor)}
+    />
+  );
 }
 
 export default function DonutIndex() {
   const [pageContainerRef, { width }] = useMeasure({ debounce: 300 });
 
-  const [color, setColor] = useState<number | null>(null);
+  const [color, setColor] = useState<number>(0xe780ce);
+  const [colorInput, setColorInput] = useState<string | undefined>(undefined);
 
-  function handleColorChange(newColor: number) {
-    setColor(newColor);
+  function handleColorChange(newValue: string, fromInput?: boolean) {
+    if (fromInput) {
+      setColorInput(newValue);
+      setColor(parseInt(newValue.replace('#', '0x'), 16));
+    }
+
+    setColor(parseInt(newValue.replace('#', '0x'), 16));
   }
 
   return (
@@ -32,18 +72,41 @@ export default function DonutIndex() {
       <div className="body-container mx-auto h-[100dvh] max-w-[var(--max-width)]">
         {width && (
           <motion.div
-            className={`donut-container fixed z-30 h-[100vh] w-full md:max-h-[800px] lg:max-h-[1000px] lg:max-w-[var(--max-width)]`}
+            className={`donut-container fixed z-30 h-[100vh] w-full px-[50px] pt-[50px] md:max-h-[800px] lg:max-h-[1000px] lg:max-w-[var(--max-width)]`}
           >
-            <ErrorBoundarySimple>
-              <Suspense fallback={<DonutFallback />}>
-                <h1>Donut</h1>
-                <input type="color" />
-                <button type="button" onClick={() => handleColorChange(0x0000ff)}>
-                  Change Color
-                </button>
-                <DonutContainer color={color} />
-              </Suspense>
-            </ErrorBoundarySimple>
+            <div className="relative flex h-full justify-end rounded-lg bg-slate-100">
+              <div className="h-[80%] w-[80%]">
+                <ErrorBoundarySimple>
+                  <Suspense fallback={<DonutFallback />}>
+                    <DonutContainer color={color} />
+                  </Suspense>
+                </ErrorBoundarySimple>
+              </div>
+              <div className="absolute left-0 top-[50%] pl-[50px] text-gray-900">
+                <h1 className="text-6xl">Donut</h1>
+                <p className="mt-[20px] max-w-[43%] text-lg text-gray-700">
+                  This is a donut I created all by myself (also with the help of watching multiple YouTube videos over
+                  and over) in Blender. I then <span>expertly</span> exported it here for use in this magnificent
+                  display.
+                </p>
+                <div className="mt-[40px] flex w-[300px] justify-between">
+                  {icingColors.map((icingColor, index) => (
+                    <ColorButton
+                      key={index}
+                      selectedColor={color}
+                      color={icingColor}
+                      handleColorChange={handleColorChange}
+                    />
+                  ))}
+                  <input
+                    id="color-input"
+                    type="color"
+                    value={colorInput}
+                    onChange={(event) => handleColorChange(event.target.value, true)}
+                  />
+                </div>
+              </div>
+            </div>
           </motion.div>
         )}
       </div>
