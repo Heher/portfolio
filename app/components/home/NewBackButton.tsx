b09468c4ddda023b29c0c2ac4368fc5097561ae3
John Heher <jheher@gmail.com>
2023-09-13 23:45:49 -0500

    Tweaks back button for mobile and adds movement

--- app/components/home/NewBackButton.tsx
+++ app/components/home/NewBackButton.tsx
@@ -1,12 +1,23 @@
 import { Link } from '@remix-run/react';
 import IndexArrow from '../icons/IndexArrow';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+
+const MotionArrow = motion(IndexArrow);
 
 export default function NewBackButton() {
+  const [hover, setHover] = useState(false);
+
   return (
-    <div className="fixed left-0 top-0 h-10 w-full bg-[rgba(15,22,26,0.7)]">
+    <div className="fixed left-0 top-0 h-[48px] w-full bg-[rgba(15,22,26,0.7)] md:h-[60px]">
       <div className="mx-auto flex h-full w-full max-w-[var(--back-button-max-width)] items-center px-5">
-        <Link to="/trip" className="px-2 py-1">
-          <IndexArrow className="w-6 rotate-180 fill-[#dddddd]" />
+        <Link
+          to="/trip"
+          className="rotate-180 px-2 py-1"
+          onMouseOver={() => setHover(true)}
+          onMouseOut={() => setHover(false)}
+        >
+          <MotionArrow className="h-4 fill-[#dddddd] md:h-5" initial={{ x: 0 }} animate={{ x: hover ? '4px' : 0 }} />
         </Link>
       </div>
     </div>
