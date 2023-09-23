c9ad79683918a1c5a29aecec1a777778387fb8e0
John Heher <jheher@gmail.com>
2023-09-23 17:09:33 -0500

    Checkpoint for moving to remix2

--- remix.config.js
+++ remix.config.js
@@ -32,13 +32,5 @@ module.exports = {
     'internmap',
     'robust-predicates'
   ],
-  tailwind: true,
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true
-  }
+  tailwind: true
 };
