0f823bf5ab305405000825d2e34d0d85026fee6e
John Heher <jheher@gmail.com>
2023-10-10 11:46:46 -0500

    Adds accessibility fixes

--- app/components/icons/PassportPlane.tsx
+++ app/components/icons/PassportPlane.tsx
@@ -3,8 +3,8 @@ export default function PassportPlane({ className }: { className?: string }) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 704.06 278.6875"
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       className={className}
     >
       <g>
