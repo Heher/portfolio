c5c7ab241f2b0f76f3fa407e3ce4d9c29a1519ee
John Heher <jheher@gmail.com>
2024-09-20 10:12:51 -0500

    Checkpoint

--- app/components/globe/shaders/globe/vertex.glsl
+++ app/components/globe/shaders/globe/vertex.glsl
@@ -19,7 +19,7 @@ void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
   modelPosition.y += sin(modelPosition.x * 5.0 + u_time * 3.0) * 0.1;
-  modelPosition.y += sin(modelPosition.z * 6.0 + u_time * 2.0) * 0.1;
+  // modelPosition.y += sin(modelPosition.z * 6.0 + u_time * 2.0) * 0.1;
 
   vZ = modelPosition.y;
 
