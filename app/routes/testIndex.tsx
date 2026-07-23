ae263a21de6c4024a0c4f506544039014b52fd64
John Heher <jheher@gmail.com>
2026-07-23 13:10:02 -0500

    Seeing if I can get a test page working

---
+++ app/routes/testIndex.tsx
@@ -0,0 +1,8 @@
+export default function TestIndex() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold">Test Index</h1>
+      <p className="text-lg text-gray-600">This is a test index page.</p>
+    </div>
+  );
+}
