484f3038d7103e5fbaec714f37837aa3486ab445
John Heher <jheher@gmail.com>
2026-07-29 23:58:22 -0500

    Checkpoint

--- app/routes/ui/onboarding.tsx
+++ app/routes/ui/onboarding.tsx
@@ -1,35 +1,27 @@
 import { createTableData } from '~/components/uis/tables/utils';
 
-import type { Route } from './+types/tables';
-
 export async function loader() {
   const tableData = createTableData(20);
 
   return { data: tableData };
 }
 
-function PlanCard({ children }: { children?: React.ReactNode }) {
+export default function OnboardingPage() {
   return (
-    <div className="relative flex h-70 w-50 rounded-lg border-2 border-transparent bg-[#F5F5F5] p-4">
-      {children}
-    </div>
-  );
-}
-
-export default function OnboardingPage({ loaderData }: Route.ComponentProps) {
-  return (
-    <div className="">
+    <div className="flex h-full min-h-full flex-col">
       <title>Onboarding | UIs | John Heher</title>
       <meta name="description" content="A simple onboarding page created by John Heher." />
-      <h1 className="text-2xl font-semibold">Onboarding</h1>
-      <p className="my-5 text-base text-[#282B27]">
-        An example of an onboarding marketing landing page with signup.
-      </p>
-      <div className="mt-10">
-        <h2 className="text-center text-xl font-semibold">Choose a plan</h2>
-        <div className="mt-5 flex justify-center gap-3">
-          <div className="group relative">
-            <div className="relative z-2 m-0.5 flex h-69 w-49 rounded-md bg-[#F5F5F5] p-4">
+      <div className="mx-auto w-full max-w-xl py-8">
+        <h1 className="text-3xl font-semibold">Onboarding</h1>
+        <p className="my-5 text-lg text-[#282B27]">
+          An example of an onboarding marketing landing page with signup.
+        </p>
+      </div>
+      <div className="h-full bg-mist-800 pt-30 pb-50">
+        <h2 className="text-center text-2xl font-semibold text-better-white">Choose a plan</h2>
+        <div className="mt-10 flex justify-center gap-3">
+          <div className="group relative text-better-black">
+            <div className="relative z-2 m-1 flex h-70 w-50 rounded-md bg-[#F5F5F5] p-4">
               <div className="relative flex flex-col justify-between bg-[#f5f5f5]">
                 <div>
                   <h2 className="text-lg">Heher+ with ads</h2>
@@ -39,13 +31,13 @@ export default function OnboardingPage({ loaderData }: Route.ComponentProps) {
               </div>
             </div>
             <div className="
-              absolute top-0 left-0 z-1 size-full rounded-lg bg-[#FF0000] transition-all
+              absolute top-0 left-0 z-1 size-full rounded-lg border border-better-white/10 bg-better-white/70 transition-all
               group-hover:blur-xs
             "
             />
           </div>
-          <div className="group relative">
-            <div className="relative z-2 m-0.5 flex h-69 w-49 rounded-md bg-[#F5F5F5] p-4">
+          <div className="group relative text-better-black">
+            <div className="relative z-2 m-1 flex h-70 w-50 rounded-md bg-[#F5F5F5] p-4">
               <div className="relative flex flex-col justify-between bg-[#f5f5f5]">
                 <div>
                   <h2 className="text-lg">Heher+</h2>
@@ -55,15 +47,15 @@ export default function OnboardingPage({ loaderData }: Route.ComponentProps) {
               </div>
             </div>
             <div className="
-              absolute top-0 left-0 z-1 size-full rounded-lg bg-[#00FF00] transition-all
+              absolute top-0 left-0 z-1 size-full rounded-lg border border-better-white bg-emerald-500 transition-all
               group-hover:blur-xs
             "
             />
           </div>
-          <div className="group relative">
-            <div className="relative z-2 m-0.5 flex h-69 w-49 rounded-md bg-[#F5F5F5] p-4">
+          <div className="group relative text-better-black">
+            <div className="relative z-2 m-1 flex h-70 w-50 rounded-md bg-[#F5F5F5] p-4">
               <div className="relative flex flex-col justify-between bg-[#f5f5f5]">
-                <div>
+                <div className="flex flex-col">
                   <h2 className="text-lg">Heher+ Bundle</h2>
                   <span className="text-sm text-[#777]">with Heher Music™</span>
                 </div>
@@ -71,7 +63,7 @@ export default function OnboardingPage({ loaderData }: Route.ComponentProps) {
               </div>
             </div>
             <div className="
-              absolute top-0 left-0 z-1 size-full rounded-lg bg-[#0000FF] transition-all
+              absolute top-0 left-0 z-1 size-full rounded-lg border border-better-white bg-linear-to-br from-emerald-400 to-fuchsia-400 transition-all
               group-hover:blur-xs
             "
             />
