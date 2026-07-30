4cb3102ddda38955d46952ec47fd1eece6ced887
John Heher <jheher@gmail.com>
2026-07-30 11:42:45 -0500

    Better styling of onboarding page

--- app/routes/ui/onboarding.tsx
+++ app/routes/ui/onboarding.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { createTableData } from '~/components/uis/tables/utils';
 
 export async function loader() {
@@ -6,7 +8,15 @@ export async function loader() {
   return { data: tableData };
 }
 
+const plans = {
+  basic: '1',
+  premium: '2',
+  bundle: '3',
+};
+
 export default function OnboardingPage() {
+  const [selectedPlan, setSelectedPlan] = useState<keyof typeof plans | null>('basic');
+
   return (
     <div className="flex h-full min-h-full flex-col">
       <title>Onboarding | UIs | John Heher</title>
@@ -17,57 +27,91 @@ export default function OnboardingPage() {
           An example of an onboarding marketing landing page with signup.
         </p>
       </div>
-      <div className="h-full bg-mist-800 pt-30 pb-50">
+      <div className="h-full bg-linear-to-b from-blue-950 to-mist-700 pt-30 pb-60">
         <h2 className="text-center text-2xl font-semibold text-better-white">Choose a plan</h2>
         <div className="mt-10 flex justify-center gap-3">
-          <div className="group relative text-better-black">
-            <div className="relative z-2 m-1 flex h-70 w-50 rounded-md bg-[#F5F5F5] p-4">
-              <div className="relative flex flex-col justify-between bg-[#f5f5f5]">
-                <div>
-                  <h2 className="text-lg">Heher+ with ads</h2>
-                  {/* <span className="text-sm text-[#777]">with ads</span> */}
-                </div>
-                <span className="text-xl">$4.99/mo</span>
+          <div className="group relative cursor-pointer text-better-black" onClick={() => setSelectedPlan('basic')} data-selected={selectedPlan === 'basic'}>
+            <div className="
+              relative z-2 m-2 flex h-70 w-55 rounded-sm bg-mist-400 p-4 transition-all
+              group-hover:not-group-data-selected:bg-mist-200
+              group-data-selected:bg-mist-50
+            "
+            >
+              <div className="relative grid grid-rows-[50px_1fr_50px]">
+
+                <h2 className="text-lg">Heher+ with ads</h2>
+                <ul className="mt-5 flex list-none flex-col gap-2 text-sm text-gray-700">
+                  <li>Watch over 100+ shows and movies with ads</li>
+                </ul>
+                <span className="self-end text-xl">$4.99/mo</span>
               </div>
             </div>
             <div className="
-              absolute top-0 left-0 z-1 size-full rounded-lg border border-better-white/10 bg-better-white/70 transition-all
-              group-hover:blur-xs
+              absolute top-0 left-0 z-1 size-full rounded-lg bg-better-white/70 transition-all
+              group-hover:not-group-data-selected:bg-better-white/80
+              group-data-selected:bg-better-white/90
             "
             />
           </div>
-          <div className="group relative text-better-black">
-            <div className="relative z-2 m-1 flex h-70 w-50 rounded-md bg-[#F5F5F5] p-4">
-              <div className="relative flex flex-col justify-between bg-[#f5f5f5]">
-                <div>
-                  <h2 className="text-lg">Heher+</h2>
-                  {/* <span className="text-sm text-[#777]">with ads</span> */}
-                </div>
-                <span className="text-xl">$9.99/mo</span>
+          <div className="group relative cursor-pointer text-better-black" onClick={() => setSelectedPlan('premium')} data-selected={selectedPlan === 'premium'}>
+            <div className="
+              relative z-2 m-2 flex h-70 w-55 rounded-sm bg-mist-400 p-4 transition-all
+              group-hover:not-group-data-selected:bg-mist-200
+              group-data-selected:bg-mist-50
+            "
+            >
+              <div className="relative grid grid-rows-[50px_1fr_50px]">
+                <h2 className="text-lg">Heher+</h2>
+                <ul className="mt-5 flex list-none flex-col gap-2 text-sm text-gray-700">
+                  <li>Watch over 100+ shows and movies ad-free</li>
+                  <li>4K picture quality</li>
+                </ul>
+                <span className="self-end text-xl">$9.99/mo</span>
               </div>
             </div>
             <div className="
-              absolute top-0 left-0 z-1 size-full rounded-lg border border-better-white bg-emerald-500 transition-all
-              group-hover:blur-xs
+              absolute top-0 left-0 z-1 size-full rounded-lg bg-emerald-700 transition-all
+              group-hover:not-group-data-selected:bg-emerald-600
+              group-data-selected:bg-emerald-400
             "
             />
           </div>
-          <div className="group relative text-better-black">
-            <div className="relative z-2 m-1 flex h-70 w-50 rounded-md bg-[#F5F5F5] p-4">
-              <div className="relative flex flex-col justify-between bg-[#f5f5f5]">
-                <div className="flex flex-col">
+          <div className="group relative cursor-pointer text-better-black" onClick={() => setSelectedPlan('bundle')} data-selected={selectedPlan === 'bundle'}>
+            <div className="
+              relative z-2 m-2 flex h-70 w-55 rounded-sm bg-mist-400 p-4 transition-all
+              group-hover:not-group-data-selected:bg-mist-200
+              group-data-selected:border-blue-300 group-data-selected:bg-mist-50
+            "
+            >
+              <div className="relative grid grid-rows-[50px_1fr_50px]">
+                <div>
                   <h2 className="text-lg">Heher+ Bundle</h2>
-                  <span className="text-sm text-[#777]">with Heher Music™</span>
+                  <span className="text-sm text-gray-600">with Heher Music™</span>
                 </div>
-                <span className="text-xl">$14.99/mo</span>
+                <ul className="mt-5 flex list-none flex-col gap-2 text-sm text-gray-700">
+                  <li>Watch over 100+ shows and movies ad-free</li>
+                  <li>4K picture quality</li>
+                  <li>Stream over 100+ million songs</li>
+                </ul>
+                <span className="self-end text-xl">$14.99/mo</span>
               </div>
             </div>
             <div className="
-              absolute top-0 left-0 z-1 size-full rounded-lg border border-better-white bg-linear-to-br from-emerald-400 to-fuchsia-400 transition-all
-              group-hover:blur-xs
+              absolute top-0 left-0 z-1 size-full rounded-lg bg-linear-to-br from-emerald-700 to-fuchsia-700 transition-all
+              group-hover:not-group-data-selected:from-emerald-600 group-hover:not-group-data-selected:to-fuchsia-600
+              group-data-selected:from-emerald-400 group-data-selected:to-fuchsia-400
             "
             />
           </div>
+        </div>
+        <div className="flex justify-center">
+          <button className="
+            mt-20 cursor-pointer rounded-lg bg-blue-950 px-6 py-3 text-lg font-semibold text-better-white transition-colors
+            hover:bg-blue-800
+          "
+          >
+            Next
+          </button>
         </div>
       </div>
     </div>
