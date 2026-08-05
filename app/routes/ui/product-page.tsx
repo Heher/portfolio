286d24ae098a9f008c9e76ce8a189a699e23c2f1
John Heher <jheher@gmail.com>
2026-08-05 16:51:28 -0500

    Product page styling for mobile

--- app/routes/ui/product-page.tsx
+++ app/routes/ui/product-page.tsx
@@ -1,7 +1,5 @@
-import { ArrowLeft } from 'lucide-react';
-import { Link } from 'react-router';
-
 import { AggroCragContainer } from '@/app/components/aggro/AggroCragContainer';
+import Header from '@/app/components/uis/Header';
 
 // export async function loader() {
 //   const tableData = createTableData(10);
@@ -11,40 +9,51 @@ import { AggroCragContainer } from '@/app/components/aggro/AggroCragContainer';
 
 export default function ProductPage() {
   return (
-    <div className="">
+    <div className="flex min-h-dvh flex-col">
       <title>Product page | UIs | John Heher</title>
       <meta name="description" content="A product page for an incredible piece of the Aggro Crag created by John Heher." />
-      <div className="mx-auto flex w-full max-w-xl flex-col pb-8">
-        <Link
-          to="/"
-          className="
-            flex w-max items-center gap-2 text-base font-semibold text-[#282B27]
-            hover:opacity-80
-          "
-        >
-          <ArrowLeft className="size-4.5" />
-          <span className="block">Back</span>
-        </Link>
-        <div className="mt-10">
-          <h1 className="text-3xl font-semibold">Product page</h1>
-          <p className="my-5 text-lg text-[#282B27]">
-            For all my fellow Millennials out there. Or simply anyone who is a fan of Nickelodeon's GUTS.
-          </p>
-        </div>
-      </div>
-      <div className="h-full bg-linear-to-b from-product-page to-product-page-dark pt-30 pb-60">
+      <Header heading="Product page" subhead="For all my fellow Millennials out there. Or simply anyone who is a fan of Nickelodeon's GUTS." />
+      <div className="
+        h-full flex-1 bg-linear-to-b from-product-page to-product-page-dark px-2.5 pt-5 pb-10
+        sm:px-0 sm:pt-30 sm:pb-0
+      "
+      >
         <div className="mx-auto w-full max-w-4xl bg-better-white p-3">
-          <div className="w-full p-7.5">
-            <div className="relative flex h-full gap-10 rounded-lg">
-              <div className="size-100 bg-slate-500">
+          <div className="
+            w-full p-3
+            sm:p-7.5
+          "
+          >
+            <div className="
+              relative flex h-full flex-col gap-5 rounded-lg
+              sm:flex-row sm:gap-10
+            "
+            >
+              <div className="
+                aspect-square w-full bg-slate-500
+                sm:size-100
+              "
+              >
                 <AggroCragContainer />
               </div>
-              <div className="flex max-w-87.5 flex-col justify-between py-5">
+              <div className="
+                flex max-w-87.5 flex-col justify-between
+                sm:py-5
+              "
+              >
                 <div>
-                  <h2 className="font-fraunces text-5xl font-semibold text-better-black">
+                  <h2 className="
+                    font-fraunces text-4xl font-semibold text-better-black
+                    sm:text-5xl
+                  "
+                  >
                     Aggro Crag
                   </h2>
-                  <p className="mt-8 text-base/relaxed text-gray-700">
+                  <p className="
+                    mt-5 font-zilla text-base/relaxed text-gray-700
+                    sm:mt-8
+                  "
+                  >
                     Never got a chance to test your skills at Slam Dunk, Moon Race, or the always frustrating Boogie Down? No need to go to Mo for a score update when this incredible, priceless, and of course glowing piece of the radical
                     rock is just a click away.
                   </p>
@@ -52,8 +61,9 @@ export default function ProductPage() {
                 <button
                   type="button"
                   className="
-                    mt-15 w-full cursor-pointer rounded-lg bg-product-page px-5 py-3 text-lg font-semibold text-better-white transition-colors
+                    mt-15 w-full cursor-pointer rounded-lg bg-product-page px-5 py-3 text-base font-semibold text-better-white transition-colors
                     hover:bg-product-page-dark
+                    sm:text-lg
                   "
                 >
                   Add to cart
@@ -61,9 +71,26 @@ export default function ProductPage() {
               </div>
             </div>
             <div className="my-10 h-0.5 w-full bg-gray-300" />
-            <div className="flex items-center gap-14 px-20">
-              <span className="block pl-10 text-center font-serif text-3xl">&ldquo;My life was truly complete once I had the piece of our radical rock in my home.&rdquo;</span>
-              <a href="/aggro.usdz" rel="ar" className="w-56 shrink-0">
+            <div className="
+              flex items-center
+              sm:gap-14 sm:px-20
+            "
+            >
+              <span className="
+                relative z-2 -mr-8 block bg-better-white p-3 text-center font-serif text-lg font-semibold
+                sm:pl-10 sm:text-3xl
+              "
+              >
+                &ldquo;My life was truly complete once I had the piece of our radical rock in my home.&rdquo;
+              </span>
+              <a
+                href="/aggro.usdz"
+                rel="ar"
+                className="
+                  w-1/2 shrink-0
+                  sm:w-56
+                "
+              >
                 <img src="/images/guts.jpeg" alt="Guy with guts" className="" />
               </a>
             </div>
