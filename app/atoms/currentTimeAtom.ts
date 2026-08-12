1f95d61ebcd9c9e733d08171f7d5b5fbddeff81a
John Heher <jheher@gmail.com>
2026-08-12 18:07:51 -0500

    Adds current time marker and scrolls to it on load

--- app/atoms/currentTimeAtom.ts
+++ app/atoms/currentTimeAtom.ts
@@ -1,6 +1,6 @@
 import { atom } from 'jotai';
 
-export const currentTimeState = atom({
-  time: '00:00:00',
+export const currentTimeState = atom<{ time: string | null; left: number }>({
+  time: null,
   left: 0,
 });
--- app/components/demos/tv-guide/CurrentTimeLine.tsx
+++ app/components/demos/tv-guide/CurrentTimeLine.tsx
@@ -6,8 +6,8 @@ export default function CurrentTimeLine() {
   const currentTime = useAtomValue(currentTimeState);
 
   return (
-    <div className="absolute top-0 z-3 h-full" style={{ left: currentTime.left - 3 }}>
-      <div className="absolute top-0 h-full w-0.75 bg-blue-700/50" />
+    <div className="absolute top-0 h-full" style={{ left: currentTime.left - 3 }}>
+      <div className="absolute top-0 z-3 h-full w-0.75 bg-blue-700/50" />
     </div>
   );
 }
