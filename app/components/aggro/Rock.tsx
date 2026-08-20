51d629073c55d5cc900e90db3f58667d39e43932
John Heher <johnheher@gmail.com>
2026-08-20 09:48:53 -0500

    Updates aggro file to be more performant (#803)

--- app/components/aggro/Rock.tsx
+++ app/components/aggro/Rock.tsx
@@ -1,26 +1,13 @@
-import type { Mesh } from 'three';
-import type { GLTF } from 'three-stdlib';
+import type { BufferGeometry } from 'three';
 
-import { useGLTF } from '@react-three/drei';
-
-type RockGLTF = GLTF & {
-  nodes: {
-    Rock: Mesh;
-  };
-};
-
-export default function Rock() {
-  const { nodes } = useGLTF('/gltf/aggro/rock.gltf') as unknown as RockGLTF;
-
+export default function Rock({ geometry }: { geometry: BufferGeometry }) {
   return (
     <group dispose={null}>
       <mesh
         castShadow
-        geometry={nodes.Rock.geometry}
-        position={[0, 80, 15]}
-        scale={0.2}
+        geometry={geometry}
+        position={[0, 0.693, 0.109]}
       >
-        {/* <meshBasicMaterial color={[0.34 * 1, 0.57 * 1, 0.23 * 1]} /> */}
         <meshBasicMaterial color={[0.34 * 2.24, 0.57 * 2.24, 0.23 * 2.24]} />
       </mesh>
     </group>
