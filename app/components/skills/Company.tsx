b02b9c760b84a121fc138a61311871096d72bf82
John Heher <johnheher@gmail.com>
2026-08-20 10:52:40 -0500

    Styling fixes for skills (#805)

--- app/components/skills/Company.tsx
+++ app/components/skills/Company.tsx
@@ -23,15 +23,17 @@ export default function Company({
       data-company={info.slug}
     >
       <div className={cn(`
-        relative w-[260px] rounded-sm border-2 border-name bg-better-white px-7 py-5 opacity-40 transition-opacity
-        sm:w-[380px]
-      `, selected && `opacity-100`)}
+        relative w-75 rounded-sm border-2 border-name/40 bg-better-white/40 px-7 py-5
+        sm:w-95
+      `, selected && `border-name bg-better-white`)}
       >
-        <div className={cn(`absolute top-1/2 -left-8 z-1 size-6 -translate-y-1/2 rounded-full border border-better-black bg-better-white`)}>
-          <span className="absolute top-1/2 left-1/2 z-2 size-3 -translate-1/2 rounded-full bg-better-black" />
+        <div className={cn(`absolute top-1/2 -left-8.5 z-1 size-6 -translate-y-1/2 rounded-full border border-name bg-[oklch(0.9302_0.0011_197.14)] transition-colors`, selected && `
+          bg-better-white
+        `)}
+        >
+          <span className={cn(`absolute top-1/2 left-1/2 z-2 size-3 -translate-1/2 rounded-full bg-[oklch(0.6711_0.015_258.36)] transition-colors`, selected && `bg-name`)} />
         </div>
-        {/* <div className="grid grid-cols-[50px_1fr] items-center"> */}
-        <div className="flex gap-5">
+        <div className={cn(`flex gap-5 opacity-40 transition-opacity`, selected && `opacity-100`)}>
           <img src={companyLogos[info.slug]} alt={`${info.name} logo`} className="h-8" />
           <div>
             <h3 className="
@@ -41,10 +43,10 @@ export default function Company({
             >
               {info.name}
             </h3>
-            <p className="mt-1 max-w-[300px] text-base">{info.location}</p>
+            <p className="mt-1 max-w-75 text-base">{info.location}</p>
           </div>
         </div>
-        <p className="mt-5 max-w-[300px] font-zilla text-lg font-medium">{info.title}</p>
+        <p className={cn(`mt-5 max-w-75 font-zilla text-lg font-medium opacity-40 transition-opacity`, selected && `opacity-100`)}>{info.title}</p>
       </div>
     </div>
   );
